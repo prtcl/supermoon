8d1f137a3b069f24cd8b393946044d4cd6bcd762
Cory O'Brien <cory@prtcl.cc>
2015-10-06 19:34:30 -0400

    remove ogg and the not finished mp3 streamer for now

--- app/lib/radio-streamer.js
+++ app/lib/radio-streamer.js
@@ -1,9 +1,7 @@
 
 var uid2 = require('uid2'),
     EventEmitter = require('events'),
-    request = require('request'),
-    ogg = require('ogg'),
-    lame = require('lame');
+    request = require('request');
 
 var _ = {
     bind: require('lodash/function/bind'),
@@ -48,6 +46,45 @@ OggStream.prototype.stop = function () {
     this.emit('stop');
     return this;
 };
+
+// function Mp3Stream (url, req, res) {
+//     this._running = false;
+//     this.id = uid2(8);
+//     this.url = url;
+//     this.type = mimeTypes['mp3'];
+//     this.client = { req: req, res: res };
+// }
+
+// Mp3Stream.prototype = _.create(EventEmitter.prototype, Mp3Stream.prototype);
+
+// Mp3Stream.prototype.start = function () {
+//     if (this._running) return this;
+//     function responseHandler (res) {
+//         this._running = true;
+//         this.res = res;
+//         this.client.res.status(200).type(this.type);
+//         this.oggDecoder = new ogg.Decoder();
+//         this.res.pipe(this.oggDecoder);
+//         this.oggDecoder.on('stream', function (stream) {
+//             console.log(stream);
+//         });
+//         // this.res.pipe(this.client.res);
+//         this.emit('start');
+//     }
+//     this.client.req.on('close', _.bind(this.stop, this));
+//     this.req = request(this.url)
+//         .on('response', _.bind(responseHandler, this));
+//     return this;
+// };
+
+// Mp3Stream.prototype.stop = function () {
+//     this._running = false;
+//     this.client.res.end();
+//     this.req && this.req.abort();
+//     this.res && this.res.destroy();
+//     this.emit('stop');
+//     return this;
+// };
 
 function RadioStreamer () {
     this.streams = [];
