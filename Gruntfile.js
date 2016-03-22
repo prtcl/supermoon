dc0c5eedace1e946c8f35195f52eacad63d0e4d5
Cory O'Brien <cory@prtcl.cc>
2016-03-22 01:09:47 -0400

    add autoprefixer/postcss to grunt build, cleanup sass and templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,66 +1,87 @@
 
 module.exports = function (grunt) {
 
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        options: { debug: false },
-        pkg: grunt.file.readJSON('package.json'),
-        browserify: {
-            app: {
-                src: ['app/client.js'],
-                dest: 'static/build/client.js',
-                options: {
-                    paths: ['./node_modules','./app'],
-                    browserifyOptions: {
-                        debug: true
-                    }
-                }
-            }
-        },
-        sass: {
-            options: {
-                sourceMap: true
-            },
-            dist: {
-                files: {
-                    'static/build/main.css': 'scss/main.scss'
-                }
-            }
-        },
-        nodemon: {
-            dev: {
-                script: 'index.js'
-            }
-        },
-        concurrent: {
-            dev: {
-                tasks: ['nodemon', 'watch'],
-                options: {
-                    logConcurrentOutput: true
-                }
-            }
-        },
-        watch: {
-            app: {
-                files: ['app/**/*.js'],
-                tasks: ['build'],
-                options: {
-                    spawn: false,
-                }
-            },
-            sass: {
-                files: ['scss/**/*.scss'],
-                tasks: ['build'],
-                options: {
-                    spawn: false
-                }
-            }
+  grunt.initConfig({
+    options: { debug: false },
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      app: {
+        src: ['app/app.js'],
+        dest: 'static/build/app.js',
+        options: {
+          paths: ['./node_modules','./app'],
+          browserifyOptions: {
+            debug: true
+          }
         }
-    });
+      }
+    },
+    uglify: {
+      options: {
+        sourceMap: true,
+        sourceMapIncludeSources: true
+      },
+      app: {
+        src: 'static/build/app.js',
+        dest: 'static/build/app.js'
+      }
+    },
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'static/build/main.css': 'scss/main.scss'
+        }
+      }
+    },
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')({ browsers: ['last 2 versions'] })
+        ]
+      },
+      dist: {
+        src: 'static/build/*.css'
+      }
+    },
+    nodemon: {
+      dev: {
+        script: 'index.js'
+      }
+    },
+    concurrent: {
+      dev: {
+        tasks: ['nodemon', 'watch'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+    watch: {
+      app: {
+        files: ['app/**/*.js'],
+        tasks: ['browserify'],
+        options: {
+          spawn: false,
+        }
+      },
+      sass: {
+        files: ['scss/**/*.scss'],
+        tasks: ['sass', 'postcss'],
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
 
-    grunt.registerTask('default', []);
-    grunt.registerTask('build', ['browserify', 'sass']);
-    grunt.registerTask('develop', ['browserify', 'sass', 'concurrent:dev']);
+  grunt.registerTask('default', []);
+  grunt.registerTask('build', ['browserify', 'uglify', 'sass', 'postcss']);
+  grunt.registerTask('develop', ['browserify', 'sass', 'concurrent:dev']);
 
 };
